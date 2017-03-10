b1f2c387192b3d6b13e5872c57c885568365f32c
jakkawad <sunnysunoc@gmail.com>
2017-03-10 10:41:19 +0700

    Initial Commit

---
+++ AlamofireH1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlamofireH1
+//
+//  Created by admin on 3/10/2560 BE.
+//  Copyright © 2560 Jakkawad.Chaiplee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
